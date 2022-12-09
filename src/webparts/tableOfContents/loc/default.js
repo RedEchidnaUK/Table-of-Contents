1c8e47ccd3a7496beb9e34dd378df6ce43237334
Constantin Lotz <Constantin.Lotz@ruv-bkk.de>
2022-12-09 11:59:16 +0100

    Added possibility to show Sections within the ToC

---
+++ src/webparts/tableOfContents/loc/default.js
@@ -0,0 +1,33 @@
+/**
+ * File: c:\dev\Table-of-Contents\lib\webparts\tableOfContents\loc\de-de.js
+ * Project: c:\dev\Table-of-Contents
+ * Created Date: Monday, December 5th 2022, 4:30:45 pm
+ * Author: Constantin Lotz
+ * -----
+ * Last Modified: Fri Dec 09 2022
+ * Modified By: Constantin Lotz
+ * -----
+ * ------------------------------------
+ * Javascript will save your soul!
+ */
+
+
+ define([], function() {
+  return {
+    "propertyPaneDescription": "Auswählen welche Überschriften im Inhaltsverzeichnis aufgelistet werden sollen.",
+    "titleDefaultValue": "Inhaltsverzeichnis",
+    "titleFieldDescription": "Text to display as the Table of Contents heading",
+    "hideTitleFieldLabel": "Verstecke Überschrift",
+    "showHeading1FieldLabel": "Zeige Überschriften 1",
+    "showHeading2FieldLabel": "Zeige Überschriften 2",
+    "showHeading3FieldLabel": "Zeige Überschriften 3",
+    "showPreviousPageViewLabel": "Zeige Link zur vorherigen Seite",
+    "previousPageFieldLabel": "Link Text",
+    "previousPageFieldDescription" : "Anzeigetext auf dem Link zur vorherigen Seite. Dieser Link ist immer der letzte Link.",
+    "previousPageDefaultValue": "Zurück zur vorherigen Seite",
+    "enableStickyModeLabel": "Aktiviere 'Sticky Mode'",
+    "enableStickyModeDescription": "Sticky Mode fixiert das Inhaltsverzeichnis am oberen Rand der Seite. Es ist automatisch bei kleinen mobilen Geräten deaktiviert. Mit vorsicht zu benutzten, da es unerwartete Nebeneffekte haben kann.",
+    "hideInMobileViewLabel": "Verstecke auf Mobilen Geräten",
+    "errorToggleFieldEmpty": "Das Feld kann nicht leer sein, wenn du das Feld nicht sehen willst, kannst du es per Toggle deaktivieren!",
+  }
+});
